c7b430827013934ec9de13cee05607b7515c8d7e
Dragon3DGraff <dragon3dgraff@mail.ru>
2022-02-28 00:11:59 +0300

    test: add multiple players

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -9,8 +9,6 @@ const web3 = new Web3(ganache.provider())
 let lottery
 let accounts
 
-console.log('Начинаю тесты');
-
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts()
 
@@ -24,7 +22,7 @@ describe('Lottery Contract', () => {
     assert.ok(lottery.options.address)
   })
 
-    it('allows enter to contract', async () => {
+    it('allows enter one player to contract', async () => {
       await lottery.methods.enter().send({
         from: accounts[0],
         value: web3.utils.toWei('0.02', 'ether')
@@ -37,4 +35,27 @@ describe('Lottery Contract', () => {
       assert.equal(accounts[0], players[0])
       assert.equal(1, players.length)
     })
+  it('allows enter multiple players to contract', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    })
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    })
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    })
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    })
+
+    assert.equal(accounts[0], players[0])
+    assert.equal(accounts[1], players[1])
+    assert.equal(accounts[2], players[2])
+    assert.equal(3, players.length)
+  })
 })
